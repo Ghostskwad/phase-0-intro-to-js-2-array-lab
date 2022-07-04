10d069c3fceaf8f05edb4979ecba1264a838c7b0
Quade Houston <quadehouston@gmail.com>
2022-07-03 22:55:09 -0400

    commit tree 951abaad16c9df8878c4da52990c7197395bfb92

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(a) {
+    return cats.push(a)
+}
+function destructivelyPrependCat(b) {
+    return cats.unshift(b)
+}
+function destructivelyRemoveLastCat(c) {
+    return cats.pop(c)
+}
+function destructivelyRemoveFirstCat(d) {
+    return cats.shift(d)
+}
+function appendCat(e) {
+    let copyCats
+    copyCats = [...cats, e]
+    return copyCats
+}
+function prependCat(f) {
+    let copyCats
+    copyCats = [f, ...cats]
+    return copyCats
+}
+function removeLastCat() {
+    return cats.slice(0, -1)
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
